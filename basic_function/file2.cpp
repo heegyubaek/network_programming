92c1f6f3075628d2c4f2da853e5925c1601b029d
heegyubaek <bhg.king47@gmail.com>
2024-07-22 17:20:13 +0900

    [Extern] 주석 추가

--- basic_function/file2.cpp
+++ basic_function/file2.cpp
@@ -3,6 +3,8 @@
 
 void modifyGlobalVar()
 {
+    // 지역 변수로 동일한 변수명을 선언시, extern 선언한 globalVar은 변화가 없다.
+    // int globalVar = 20;
     globalVar = 20;
     std::cout << "Global Variable in file2: " << globalVar << std::endl;
 }
--- basic_function/global.h
+++ basic_function/global.h
@@ -3,6 +3,8 @@
 
 extern int globalVar;
 
+// C++에서 함수의 선언은 기본적으로 외부에서 정의된다는 의미를 내포하고 있음
+// extern 키워드를 추가하나 안하나 동일한 효과를 가짐
 void printGlobalVar();
 
 #endif
