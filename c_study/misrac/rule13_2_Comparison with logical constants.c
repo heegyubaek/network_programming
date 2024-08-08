eb40a07ddd9ba6e9e823ff7101a1d646beeb95f6
Benjamin <bhg.king47@gmail.com>
2024-08-09 08:52:51 +0900

    [misraC] Add comparison with logical constants example

---
+++ c_study/misrac/rule13_2_Comparison with logical constants.c
@@ -0,0 +1,21 @@
+/* Rule 13.2: 논리적 상수와의 비교
+   논리적 상수와의 비교는 반드시 명시적으로 수행해야 하며, 묵시적인 비교는 피해야 함    
+ */
+
+ #include <stdio.h>
+
+ bool func(bool flag) {
+    // if(flag) {  //MISRA 위반: 암시적인 true 비교 수행함
+    if(flag == true) {  //수정: 명시적인 true 비교 수행해야함
+        return true;
+    }
+    return false;
+ }
+
+ void main() {
+    bool flag = false;
+
+    func(flag);
+
+    return 1;
+ }
